14a67fff71e8a9c73d63e0fdc4bd7ea3377b00c6
Jonas Juffinger <mail@jonasjuffinger.com>
2016-03-18 16:47:25 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,27 @@
+//------------------------------------------------------------------------------
+// main.cpp
+//
+// Group: Group 43, study assistant: Max Mustertutor
+//
+// Authors: Johannes Kopf (0800001)
+// Jonas Juffinger (1531253)
+// Matthias Klotz (0800003)
+//------------------------------------------------------------------------------
+//
+
+#include <stdlib.h>
+#include <iostream>
+
+//------------------------------------------------------------------------------
+// main function
+// entry point for execution
+// @param argc argument count
+// @param **argv argument values
+// @return return value for the OS
+//
+int main(int argc, char** argv)
+{
+  std::cout << "Hallo Welt" << std::endl;
+
+  return(EXIT_SUCCESS);
+}
