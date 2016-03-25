47b857c65bdeaafb9aa53af1db2fec94855322e3
johanneskopf <johanneskopf@gmx.at>
2016-03-26 00:10:21 +0100

    Implemented basic tiles

--- maze/Tile.h
+++ maze/Tile.h
@@ -5,10 +5,36 @@
 #ifndef MAZE_TILE_H
 #define MAZE_TILE_H
 
+#include "Vector2d.h"
+
+enum TileType
+{
+  Wall, Path, Start, Finish, Ice, Teleport, OneWay, Bonus, Quicksand
+};
+
+//enum class TileType : char
+//{
+//    Wall = '',
+//    Path = ' ',
+//    Start = 'o',<
+//    Finish = 'x',
+//    Ice = '+',
+//    Teleport = 'A-Z',
+//    OneWay = '><^v',
+//    Bonus = 'a-e',
+//    Quicksand ='f-j'
+//};
 
 class Tile
 {
+  private:
+    Vector2d position_;
+    TileType tileType_;
+    int add_;
 
+  public:
+    Tile(TileType tiletype);
+    bool enter(Vector2d sourcePosition, Vector2d newPosition);
 };
 
 
