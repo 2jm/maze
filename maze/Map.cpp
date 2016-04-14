64739164db07197374fce7cdfc69805c1bbe8b0a
Hiiias <mx5595@gmail.com>
2016-04-14 11:10:19 +0200

    map loading and validation except for the border

--- maze/Map.cpp
+++ maze/Map.cpp
@@ -3,12 +3,95 @@
 //
 
 #include "Map.h"
+#include "TileWall.h"
+#include "TileStart.h"
+#include "TilePath.h"
+#include "TileFinish.h"
+#include "TileTeleport.h"
+#include "TileBonus.h"
+#include "TileIce.h"
+#include "TileOneWay.h"
+#include "TileQuicksand.h"
+
 
 using std::string;
 
 bool Map::loadFromString(string map_string, Game &game)
 {
+  int x = 0;
+  int y = 0;
+  int string_position = 0;
+  int pair_nr;
   //jede Zeile des Spielfelds muss mit # aufhören.
+
+  while(map_string[string_position])
+  {
+    while(map_string[string_position] != '\n')
+    {
+      Vector2d tile_position(x, y);
+
+      if(map_string[string_position] == '#')
+      {
+        put(new TileWall(tile_position), tile_position);
+      }
+      else if(map_string[string_position] >= 'A' && map_string[string_position] <= 'Z')
+      {
+        pair_nr= map_string[string_position] - 65;  //character - ascii(A)
+        teleporter_pair_[pair_nr]++;
+
+        put(new TileTeleport(tile_position, map_string[string_position]), string_position);
+      }
+      else if(map_string[string_position] == 'o')
+      {
+        start_once_++;
+
+        put(new TileStart(tile_position), map_string[string_position]);
+        //set start_Tile_
+        start_tile_ = new TileStart(tile_position);
+
+      }
+      else if(map_string[string_position] == 'x')
+      {
+        end_once_++;
+
+        put(new TileFinish(tile_position, game), tile_position);
+        //set end_Tile_
+        end_tile_ = new TileFinish(tile_position, game);
+      }
+      else if(map_string[string_position] >= 'a' && map_string[string_position] <= 'e')
+      {
+        put(new TileBonus(tile_position, map_string[string_position], game), tile_position);
+      }
+      else if(map_string[string_position] >= 'f' && map_string[string_position] <= 'j')
+      {
+        put(new TileQuicksand(tile_position, map_string[string_position], game), tile_position);
+      }
+      else if(map_string[string_position] == ' ')
+      {
+        put(new TilePath(tile_position), tile_position);
+      }
+      else if(map_string[string_position] == '+')
+      {
+        put(new TileIce(tile_position), tile_position);
+      }
+      else if(map_string[string_position] == '<' || map_string[string_position] == '>' ||
+              map_string[string_position] == 'v' || map_string[string_position] == '^')
+      {
+        put(new TileOneWay(tile_position, map_string[string_position]), tile_position);
+      }
+      else
+      {
+        return false;
+      }
+
+
+      //Tile *tile = new Tile(tile_position, map_string[string_position]);
+
+      //put(a , tile_position);
+      x++;
+    }
+    y++;
+  }
 
   is_loaded_ = true;
   return true;
@@ -22,94 +105,39 @@ void Map::clear()
 
 void Map::check(std::string map_string)
 {
-  int map_char = 0;
-  int lines = 0;
+  Vector2d vector;
   int line_length;
-  int pair_nr;
-  int line_pos;
-  start_once_ = -1;
-  end_once_ = -1;
+  int column_height;
 
-  while(map_string[map_char] != '\n')  //check if the first line is a wall, and get the line_length
+/******checking the border****
+  vector = getSize();
+  line_length = vector.x();
+  column_height = vector.y();
+
+  for (int j = 0; j < line_length; j++)
   {
-    if(map_string[map_char] != '#')
-      Message::print(ResultCode::INVALID_FILE);
-    map_char++;
+
   }
+  */
 
-  line_length = map_char;
-  map_char++;  //increment to jump over the \n
-
-  while (map_char < map_string.length())
-  {
-    //check if the first sign in line is a wall
-    if(map_string[map_char] != '#')
-      Message::print(ResultCode::INVALID_FILE);
-
-
-
-    wall_only_ = 0;
-    for (line_pos = 0; line_pos < line_length; ++line_pos)
-    {
-      wall_only_=1;
-      if(map_string[map_char] == '#')    //check if only wall in one line(just important for last line)
-        wall_only_ = 0;
-      else if(map_string[map_char] >= 'A' && map_string[map_char] <= 'Z')
-      {
-        pair_nr= map_string[map_char] - 65;  //character - ascii(A)
-        teleporter_pair_[pair_nr]++;
-      }
-      else if(map_string[map_char] == 'o')
-      {
-        start_once_++;
-        //set start_Tile_
-      }
-      else if(map_string[map_char] == 'x')
-      {
-        end_once_++;
-        //set end_Tile_
-      }
-      else if(map_string[map_char] <= 'a' && map_string[map_char] <= 'z')
-      {
-        Message::print(ResultCode::INVALID_FILE);
-      }
-
-
-      map_char++;
-    }
-
-
-    //check if the last sign in line(one before the \n) is a wall
-    if(map_string[map_char-1] != '#')
-      Message::print(ResultCode::INVALID_FILE);
-    //check if line length is correct
-    if(map_string[map_char] != '\n')
-      Message::print(ResultCode::INVALID_FILE);
-    //check if exact one start-tile
-    if(start_once_ != 0)
-      Message::print(ResultCode::INVALID_FILE);
-    //check if exact one end-tile
-    if(end_once_ != 0)
-      Message::print(ResultCode::INVALID_FILE);
-    //check if if teleporter_pairs are correct
-    for(int i = 0; i < 26; i++)
-    {
-      if(teleporter_pair_[i] != -1 && teleporter_pair_[i] != 1)
-      {
-        Message::print(ResultCode::INVALID_FILE);
-        break;
-      }
-
-    }
-
-    lines++; //number of lines
-    map_char++; //increment to jump over the \n
-  }
-
-
-  //the last line needs to be a wall
-  if(wall_only_ != 0)
+  //check if exact one start-tile
+  if(start_once_ != 0)
     Message::print(ResultCode::INVALID_FILE);
+  //check if exact one end-tile
+  if(end_once_ != 0)
+    Message::print(ResultCode::INVALID_FILE);
+  //check if if teleporter_pairs are correct
+  for(int i = 0; i < 26; i++)
+  {
+    if(teleporter_pair_[i] != -1 && teleporter_pair_[i] != 1)
+    {
+      Message::print(ResultCode::INVALID_FILE);
+      break;
+    }
+
+  }
+
+
 }
 
 Tile *Map::getTeleporterPair(char name) // returns the corresponding second match of the teleporter tile with the given name
