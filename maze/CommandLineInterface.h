357187f958fe7ae76f261be5dfe038c5e5954058
Hiiias <mx5595@gmail.com>
2016-04-21 09:03:49 +0200

    completed comment headers at CommandLineInterface.h added some comments at the cpp

--- maze/CommandLineInterface.h
+++ maze/CommandLineInterface.h
@@ -20,6 +20,9 @@ class Game;
 class CommandLineInterface
 {
   private:
+//------------------------------------------------------------------------------
+// Enum with all command Names
+//
     enum Name
     {
       LOAD,
@@ -33,10 +36,32 @@ class CommandLineInterface
     const static std::string name_strings_[];
 
     Game &game_;
+
+//------------------------------------------------------------------------------
+// Calls the needed Commandmethod and prints the returned Message
+//
+// @param command string of the command, that was entered
+// @param params vector of strings of all params that were entered
+//
+// @return false if program should be quit else true
+//
     bool execute(std::string command, std::vector<std::string> params);
 
   public:
+//------------------------------------------------------------------------------
+// Constructor
+//
+// @param game reference of the actual game object
+//
+// @return no return
+//
     CommandLineInterface(Game &game);
+
+//------------------------------------------------------------------------------
+// Gets the Userinput (command and params) and evaluates it
+//
+// @return calls execute()-method
+//
     bool update();
 };
 
