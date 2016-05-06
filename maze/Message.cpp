047955d3d02ccae035d205ccddc2b4e1711624aa
johanneskopf <johanneskopf@gmx.at>
2016-05-06 14:31:30 +0200

    corrected indent of strings initialization

--- maze/Message.cpp
+++ maze/Message.cpp
@@ -16,24 +16,25 @@
 namespace Message
 {
   //----------------------------------------------------------------------------
-  const std::string strings[] = {
-          "", // print nothing on success
-          "[ERR] Out of memory.\n",
-          "[ERR] Wrong usage: ./basic [-s <filename1>] [-m <filename2>]\n",
-          "[ERR] File could not be opened.\n",
-          "[ERR] Invalid file.\n",
-          "[ERR] Invalid path.\n",
-          "[ERR] File could not be written.\n",
-          "[ERR] Invalid move.\n",
-          "[ERR] No more steps possible.\n",
-          "[ERR] Unknown command.\n",
-          "[ERR] Wrong parameter count.\n",
-          "[ERR] Wrong parameter.\n",
-          "[ERR] No maze loaded.\n",
-          "Bye!\n",
-          "Congratulation! You solved the maze.\n",
-          "Remaining Steps: ",
-          "Moved Steps: "
+  const std::string strings[] =
+  {
+    "", // print nothing on success
+    "[ERR] Out of memory.\n",
+    "[ERR] Wrong usage: ./basic [-s <filename1>] [-m <filename2>]\n",
+    "[ERR] File could not be opened.\n",
+    "[ERR] Invalid file.\n",
+    "[ERR] Invalid path.\n",
+    "[ERR] File could not be written.\n",
+    "[ERR] Invalid move.\n",
+    "[ERR] No more steps possible.\n",
+    "[ERR] Unknown command.\n",
+    "[ERR] Wrong parameter count.\n",
+    "[ERR] Wrong parameter.\n",
+    "[ERR] No maze loaded.\n",
+    "Bye!\n",
+    "Congratulation! You solved the maze.\n",
+    "Remaining Steps: ",
+    "Moved Steps: "
   };
 
   //----------------------------------------------------------------------------
